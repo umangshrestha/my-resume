1b52b6d16a29b727722c5840f5325f7f15b1479e
umang <umangshrestha09@gmail.com>
2021-10-17 14:28:53 +0545

    Updates

--- old/index.js
+++
@@ -1,9 +0,0 @@
-const showHideDetail = (divId) => {
-    if (document.getElementById(`${divId}-description`).style.display == "none") {
-        document.getElementById(`${divId}-description`).style.display = null;
-        document.getElementById(`${divId}-show-details`).innerHTML = "[hide details]"
-    } else {
-        document.getElementById(`${divId}-description`).style.display = "none";
-        document.getElementById(`${divId}-show-details`).innerHTML = "[show details]"
-    }
-}
